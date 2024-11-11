7fdd2a23aa26fc6feff75313a9eccc5b583197c7
vinoothna23 <thota.vinoothna@gmail.com>
2024-11-11 15:12:05 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -12,7 +12,7 @@ var isValid = function(s) {
 
     for (let char of s) {
         if (map[char]) {
-            stack.push(char);
+            sk.push(char);
         } else {
             if (stack.length === 0 || map[stack.pop()] !== char) {
                 return false;
