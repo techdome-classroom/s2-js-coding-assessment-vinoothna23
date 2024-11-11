72707cacb0b70267e8712ebdb556f1e061433448
vinoothna23 <thota.vinoothna@gmail.com>
2024-11-11 15:14:43 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -15,32 +15,7 @@ module.exports={romanToInt}
  */
 var romanToInt = function(s) {
     // Mapping of Roman numerals to integers
-    const romanMap = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
     
-    let total = 0;
-    let prevValue = 0;
-    
-    // Iterate from the end to the beginning
-    for (let i = s.length - 1; i >= 0; i--) {
-        const current = romanMap[s[i]];
-        
-        if (current < prevValue) {
-            total -= current;
-        } else {
-            total += current;
-            prevValue = current;
-        }
-    }
-    
-    return total;
 };
 
 module.exports = { romanToInt };
