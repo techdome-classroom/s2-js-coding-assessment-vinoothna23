decf21d4c8a4d4d39123dd774f603b7ff1d8d87c
vinoothna23 <thota.vinoothna@gmail.com>
2024-11-11 15:15:55 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,12 +4,7 @@
  */
 var romanToInt = function(s) {
     const romanMap = {
-        'I': 1,'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+        'I': 1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     };
     
     let total = 0;
